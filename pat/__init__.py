0bbc1137b8f290755337cc4406798b46ec3d8561
Andrew Kubera <andrewkubera@gmail.com>
2015-08-17 16:06:59 -0400

    Added initial pat module directory+__init__ script

---
+++ pat/__init__.py
@@ -0,0 +1,13 @@
+#
+# pat/__init__.py
+#
+"""
+Pat module file.
+"""
+
+
+__version__ = '0.0.0'
+__date__ = 'Aug 17, 2015'
+__author__ = 'Andrew Kubera'
+__author_email__ = 'andrewkubera@gmail.com'
+__license__ = '???'
