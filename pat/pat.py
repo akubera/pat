1667f1b3bd4c533e84d7cbf752070621533f7e5b
Andrew Kubera <andrewkubera@gmail.com>
2015-08-20 20:10:13 -0400

    pat: Added the initial pat file containing an empty Pat class.

---
+++ pat/pat.py
@@ -0,0 +1,17 @@
+#
+# pat/pat.py
+#
+"""
+Pat module providing the 'main' pat repo-accessor-class, Pat
+"""
+
+
+class Pat:
+    """
+    The main class of the pat project - providing the abstraction and interface
+    around pat/dat repositories.
+    """
+
+    def __init__(self):
+        """Constructor"""
+        pass
