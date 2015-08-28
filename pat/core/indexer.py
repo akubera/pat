ae4dc85028ce0f15699dc31f7d686e87156b3c9b
Andrew Kubera <andrewkubera@gmail.com>
2015-08-28 19:57:01 -0400

    Added a ported (very untested) Indexer class

---
+++ pat/core/indexer.py
@@ -0,0 +1,128 @@
+#
+# pat/core/indexer.py
+#
+
+import os
+from platform import system
+
+
+class Indexer:
+    """
+    I have no idea what this does!
+    """
+
+    sockpath_default_platform =
+
+    db = None
+    db_path = ''
+    checkout = None
+
+    data = None
+    changes = 0
+
+    main_layer = None
+    heads = None
+    _pending = None
+    _looping = None
+
+    def __init__(self, path='.', db=None, multiprocess=False, backend=None):
+
+        self.db_path = os.path.join(path, 'leveldb')
+
+        self._pending = []
+
+        if db is not None:
+            # ready(db, create_log(db), true)
+        elif multiprocess is None and path is None or backend is not None:
+            db = levelup(dbpath, {'db': backend})
+            # ready(db, create_log(db), true)
+
+        if system() == 'Windows':
+            path =
+{
+    'Windows': r'\\.\pipe\dat\',
+    'Linux': path.join
+}
+
+        self.multiprocess = multiprocess({
+            'sockpath': self.sockpath_default_platform[system()]
+        })
+
+
+    def create_log(self, db):
+        meta = sublevel(db, 'meta')
+
+        log = meta.get('log')
+
+        if log && log.split(':')[0:-1].join(':'), path:
+            return on_log()
+
+        log = path + ":" + cuid()
+
+        meta.put('log', log)
+
+
+        return hyperlog(sublevel(db, 'log'), {'getId': getId})
+
+
+    def create_db(self):
+        return levelup(self.dbpath, {db: self.backend})
+
+
+    def add(self, links, value):
+
+        node = self.log.add(links, messages.Commit.encode(value)
+        node, layer = self._flush_node(node)
+        if self.checkout or self.checkout in node.links:
+            self.meta.batch([{'type': 'put',
+                              'key': 'layer'
+                              'value': layer},
+                             {'type': 'del',
+                              'key': 'checkout'}])
+            self.main_layer = layer
+        return node, layer
+
+    def get(self, hash_):
+        return self.log.get(hash_)
+
+    def flush(self):
+        if self.changes < self.log.changes:
+            self._pending.append([self.log.changes, None])
+
+    def _flush_node(self, node):
+        if self.changes < node.change:
+            self._pending.append([node.change, node])
+        else:
+            layer = self.layers.get(node.key)
+            return node, layer
+
+
+    def change_checkout(self, head):
+
+        if head:
+            self.meta.put('checkout', head)
+        else:
+            self.meta.del('checkout')
+
+        self.checkout = head or None
+
+    def expand(self, hash_):
+
+        if len(hash_) < 16:
+            raise ValueError("Checkout hash must be at least 16 characters")
+        elif len(hash_) >= 64:
+            return self.get(hash_)
+
+        db = sublevel(subleve(self.db, 'log'), 'nodes')
+
+        keys = db.create_key_stream({
+            'gt': hash_,
+            'lt': hash_ + '\xff',
+            'limit': 1
+        })
+
+        result = keys()
+
+        if not result:
+            raise Exception("Could not expand the hash")
+        return self.get(result)
